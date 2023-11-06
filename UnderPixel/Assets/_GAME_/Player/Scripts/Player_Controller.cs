bb4837ebce289ae4405c5aa2126c1fe925e440d9
studentlivinginamerica704 <142178968+studentlivinginamerica704@users.noreply.github.com>
2023-11-05 19:08:11 -0500

    Map

--- UnderPixel/Assets/_GAME_/Player/Scripts/Player_Controller.cs
+++ UnderPixel/Assets/_GAME_/Player/Scripts/Player_Controller.cs
@@ -44,5 +44,27 @@ public class Player_Controller : MonoBehaviour
 	{
 		_rb.velocity = _moveDir * _moveSpeed * Time.fixedDeltaTime;
 	}
-	#endregion
+    #endregion
+
+    #region Collisions
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the collision involves a specific tag (optional).
+        if (collision.gameObject.CompareTag("Collisions"))
+        {
+            Debug.Log("Player");
+        }
+
+        // You can access information about the collision, such as contact points and the other object involved.
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.Log("Collision Contact Point: " + contact.point);
+        }
+
+        // You can also access the other GameObject involved in the collision.
+        GameObject otherObject = collision.gameObject;
+        Debug.Log("Collided with: " + otherObject.name);
+    }
+
+    #endregion
 }
